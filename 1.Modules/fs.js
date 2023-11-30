dd044b1b1880e64e7137dcb01cb967f66a3d24b0
nishantsinha1197 <nishantsinha1197@gmail.com>
2023-11-30 12:48:02 +0530

    My first commit - modules in node

---
+++ 1.Modules/fs.js
@@ -0,0 +1,16 @@
+const { readFileSync, writeFileSync } = require("fs");
+
+//Sync method
+console.log('start');
+const first = readFileSync("../content/first.txt", "utf-8");
+const second = readFileSync("../content/second.txt", "utf-8");
+
+console.log(first, second);
+
+writeFileSync(
+  "../content/result-sync.txt",
+  `Here is the result: ${first},${second}`,
+  {flag:'a'} //For appending
+);
+console.log('done with this task');
+console.log('starting the next one');
